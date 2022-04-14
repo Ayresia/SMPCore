e3c7837aa22105ecdfb8e88520be9bb4552255f2
Ayresia <nalizyoncatania0@gmail.com>
2022-04-14 19:35:29 +0200

    feat: heart scale by death/kills

--- src/main/java/me/ayresia/smpcore/smpcore/events/PlayerListener.java
+++ src/main/java/me/ayresia/smpcore/smpcore/events/PlayerListener.java
@@ -1,6 +1,7 @@
 package me.ayresia.smpcore.smpcore.events;
 
 import net.kyori.adventure.text.Component;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,6 +23,21 @@ public class PlayerListener implements Listener {
                     attacker.getName(),
                     getFormattedWeaponString(attacker)
             );
+
+            if (victim != attacker) {
+                double victimHealth = victim.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+                double attackerHealth = attacker.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+
+                if (attackerHealth < 40) {
+                    attacker.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(attackerHealth + 2);
+                    attacker.setHealthScale(attackerHealth + 2);
+                }
+
+                if (victimHealth > 10) {
+                    victim.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(victimHealth - 2);
+                    victim.setHealthScale(victimHealth - 2);
+                }
+            }
 
             event.deathMessage(Component.text(formattedDeathMessage));
             return;
