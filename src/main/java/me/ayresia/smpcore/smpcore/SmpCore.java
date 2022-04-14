467c2a568948a6f3d9080a7a21b91aa371ce6019
Ayresia <nalizyoncatania0@gmail.com>
2022-04-14 11:51:08 +0200

    feat: initial commit

---
+++ src/main/java/me/ayresia/smpcore/smpcore/SmpCore.java
@@ -0,0 +1,15 @@
+package me.ayresia.smpcore.smpcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmpCore extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
